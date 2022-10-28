4624e9d6f0cac204eba1af6a52061b60df12f305
marusosaassi <soassi@outlook.com>
2022-10-28 09:05:33 -0500

    setting up expectations + call method under test and assert results

--- src/test/java/com/luv2code/test/MockAnnotationTest.java
+++ src/test/java/com/luv2code/test/MockAnnotationTest.java
@@ -6,11 +6,16 @@ import com.luv2code.component.models.CollegeStudent;
 import com.luv2code.component.models.StudentGrades;
 import com.luv2code.component.service.ApplicationService;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
 
 @SpringBootTest(classes = MvcTestingExampleApplication.class) //we haveto do this because our packages names are different
 public class MockAnnotationTest {
@@ -38,5 +43,20 @@ public class MockAnnotationTest {
         studentOne.setLastname("Sosa");
         studentOne.setEmailAddress("soassi@outlook.com");
         studentOne.setStudentGrades(studentGrades);
+    }
+
+    @DisplayName("When & Verify")
+    @Test
+    public void assertEqualsTestAddGrades() {
+        when(applicationDao.addGradeResultsForSingleClass(
+            studentGrades.getMathGradeResults()
+        )).thenReturn(90.00);
+
+        assertEquals(90.00, applicationService
+            .addGradeResultsForSingleClass(
+                studentOne.getStudentGrades().getMathGradeResults()
+            ));
+
+
     }
 }
